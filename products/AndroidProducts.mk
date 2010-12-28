8844a029c7b91e333ec9189c480e8ab2af6e112f
Polytheus <polytheus@googlemail.com>
2010-12-28 01:13:51 +0100

    Add cyanogen_eve product
    Change-Id: I88e00db1504f22a4f893c6cfe781e0bee011aff9

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -7,6 +7,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/cyanogen_crespo.mk \
     $(LOCAL_DIR)/cyanogen_espresso.mk \
     $(LOCAL_DIR)/cyanogen_hero.mk \
+    $(LOCAL_DIR)/cyanogen_eve.mk \
     $(LOCAL_DIR)/cyanogen_inc.mk \
     $(LOCAL_DIR)/cyanogen_generic.mk \
     $(LOCAL_DIR)/cyanogen_glacier.mk \
